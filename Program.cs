977aff182111a56c40dddc620bcbdbf79d2764e6
Lune200 <nekomoon2003@gmail.com>
2023-03-31 20:42:23 -0300

    Frutas Preferidas. Feito

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Frutas preferidas");
+Console.ForegroundColor = ConsoleColor.DarkYellow;
+Console.WriteLine("1- Manga");
+Console.ForegroundColor = ConsoleColor.DarkGreen;
+Console.WriteLine("2- Melancia");
+Console.ForegroundColor = ConsoleColor.DarkRed;
+Console.WriteLine("3- Maçã");
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("4- Morango");
+Console.ForegroundColor = ConsoleColor.Magenta;
+Console.WriteLine("5- Uva");
+Console.ForegroundColor = ConsoleColor.DarkYellow;
+Console.WriteLine("6- Abacaxi");
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("7- Banana");
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("8- Kiwi");
+Console.ForegroundColor = ConsoleColor.White;
